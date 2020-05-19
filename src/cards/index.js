833b83876512de545315066ee6ec3b5754d6bea9
Chris Fontenot <chris.fontenot@gmail.com>
2020-05-19 17:46:49 -0500

    added buildings sample card back into package

--- src/cards/index.js
+++ src/cards/index.js
@@ -3,7 +3,9 @@ import React from "react";
 
 
 import StudentChargesCard from './StudentChargesCard';
+import BuildingsCard from './BuildingsCard';
 
 export default [
-    <StudentChargesCard type="StudentChargesCard" />
+    <StudentChargesCard type="StudentChargesCard" />,
+    <BuildingsCard type = "BuildingsCard" />
 ];
